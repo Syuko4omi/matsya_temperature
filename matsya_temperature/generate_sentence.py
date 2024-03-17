405ae4806652c58ad7e7b3973845cbe0272f8b5f
Syuko4omi <riorioblackcat@yahoo.co.jp>
2024-03-17 22:53:40 +0900

    ♻️ divided into several codes

--- matsya_temperature/generate_sentence.py
+++ matsya_temperature/generate_sentence.py
@@ -1,143 +1,46 @@
 import random
-import mojimoji
 from get_temperature_and_month import get_temperature_and_month
 from matsya_class import Matsya
+from text_process_funcs import process_tokens
+from specific_token_generator import this_is_gen, decorator_gen, desune_gen
 import config
 
 
-def generate_sentence():
-    # temperature, month = get_temperature_and_month()
-    temperature, month = -1.2, 1
+def generate_sentence() -> str:
+    """
+    Returns:
+        sentence (str): matsya-like sentence
+
+    Examples:
+        >>> generate_sentence()
+        さﾑ
+    """
+    temperature, month = get_temperature_and_month()
     if temperature == -1000.0:
         return random.choice(config.error_phrase)
 
     matsya = Matsya(temperature, month)
-    if random.random() < 0.78:
+    random_value = random.random()
+
+    if random_value < 0.8:
         return generate_sentence_climate(matsya.climate)
-    elif random.random() < 0.98:
+    elif random_value < 0.99:
         return generate_sentence_season(matsya.season)
     else:
         return random.choice(config.template_phrase)
 
 
-def process_tokens(tokens):
-    step_1 = remove_empty_string_from_list(tokens)
-    step_2 = add_exclamation_to_short_or_long_sentence(step_1)
-    step_3 = convert_zenkaku_katakana_to_hankaku(step_2)
-    step_4 = add_symbols(step_3)
-    return step_4
-
-
-def remove_empty_string_from_list(tokens):
-    return list(filter(lambda x: x != "", tokens))
-
-
-def add_exclamation_to_short_or_long_sentence(tokens):
-    if len(tokens) == 1 and len(tokens[0]) <= 2:
-        if random.random() < 0.3:
-            return [random.choice(config.exclamations)] + tokens
-    if len(tokens) >= 3 and sum([len(token) for token in tokens]) >= 20:
-        if random.random() < 0.3:
-            return tokens + [random.choice(config.exclamations)]
-    return tokens
-
-
-def convert_zenkaku_katakana_to_hankaku(tokens):
-    new_tokens = []
-    for token in tokens:
-        if random.random() < 0.8:
-            new_tokens.append(mojimoji.zen_to_han(token))
-        else:
-            new_tokens.append(token)
-    return new_tokens
-
-
-def add_symbols(tokens):
-    prob = 0.2
-    new_tokens = []
-    if len(tokens) == 1:
-        new_tokens.append(tokens[0])
-        if random.random() < prob:
-            new_tokens.append(symbol_gen())
-        return new_tokens
-
-    for token in tokens:
-        new_tokens.append(token)
-        new_tokens.append(symbol_gen())
-    return new_tokens
-
-
-def generate_sentence_climate(climate):
+def generate_sentence_climate(climate: str) -> str:
     prefix = this_is_gen("climate") + decorator_gen()
     climate_phrase = [random.choice(config.climate_phrase[climate])]
     postfix = desune_gen("climate")
 
-    cleaned_phrases = process_tokens(prefix + climate_phrase + postfix)
-    return "".join(cleaned_phrases)
+    return "".join(process_tokens(prefix + climate_phrase + postfix))
 
 
-def generate_sentence_season(season):
+def generate_sentence_season(season: str) -> str:
     prefix = this_is_gen("season") + decorator_gen()
     season_phrase = [random.choice(config.season_phrase[season])]
     postfix = desune_gen("season")
 
-    cleaned_phrases = process_tokens(prefix + season_phrase + postfix)
-    return "".join(cleaned_phrases)
-
-
-def this_is_gen(mode) -> list[str]:
-    random_num = random.random()
-
-    if mode == "climate":
-        if random_num < 0.8:
-            return [""]
-        else:
-            return ["これは"]
-
-    else:
-        if random_num < 0.6:
-            return ["これは"]
-        elif random_num < 0.7:
-            return ["これはね"]
-        elif random_num < 0.8:
-            return ["これはですねえ"]
-        else:
-            return [""]
-
-
-def desune_gen(mode) -> list[str]:
-    random_num = random.random()
-
-    if mode == "climate":
-        return [""]
-
-    else:
-        if random_num < 0.1:
-            return ["ネ"]
-        elif random_num < 0.2:
-            return ["です", "ネ"]
-        else:
-            return [""]
-
-
-def decorator_gen() -> list[str]:
-    decorator = random.sample(
-        config.decorators,
-        random.choices([0, 1, 2, 3], weights=[20, 5, 2, 1])[0],
-    )
-    if len(decorator) == 1:
-        if random.random() > 0.5:
-            decorator[0] = decorator[0] + "ねえ"
-    return decorator
-
-
-def symbol_gen() -> str:
-    symbols = random.choice(config.symbols)
-    while random.random() > 0.5:
-        symbols += random.choice(config.symbols)
-    return symbols
-
-
-for i in range(100):
-    text = generate_sentence()
-    print(text)
+    return "".join(process_tokens(prefix + season_phrase + postfix))
---
+++ matsya_temperature/text_process_funcs.py
@@ -0,0 +1,50 @@
+import random
+import config
+import mojimoji
+from specific_token_generator import symbol_gen
+
+
+def process_tokens(tokens: list[str]) -> list[str]:
+    step_1 = remove_empty_string_from_list(tokens)
+    step_2 = add_exclamation_to_short_or_long_sentence(step_1)
+    step_3 = convert_zenkaku_katakana_to_hankaku(step_2)
+    step_4 = add_symbols(step_3)
+    return step_4
+
+
+def remove_empty_string_from_list(tokens: list[str]) -> list[str]:
+    return list(filter(lambda x: x != "", tokens))
+
+
+def add_exclamation_to_short_or_long_sentence(tokens: list[str]) -> list[str]:
+    if len(tokens) == 1 and len(tokens[0]) <= 2:
+        if random.random() < 0.3:
+            return [random.choice(config.exclamations)] + tokens
+    if len(tokens) >= 3 and sum([len(token) for token in tokens]) >= 20:
+        if random.random() < 0.3:
+            return tokens + [random.choice(config.exclamations)]
+    return tokens
+
+
+def convert_zenkaku_katakana_to_hankaku(tokens: list[str]) -> list[str]:
+    new_tokens = []
+    for token in tokens:
+        if random.random() < 0.8:
+            new_tokens.append(mojimoji.zen_to_han(token))
+        else:
+            new_tokens.append(token)
+    return new_tokens
+
+
+def add_symbols(tokens: list[str]) -> list[str]:
+    new_tokens = []
+    if len(tokens) == 1:
+        new_tokens.append(tokens[0])
+        if random.random() < 0.2:
+            new_tokens.append(symbol_gen())
+        return new_tokens
+
+    for token in tokens:
+        new_tokens.append(token)
+        new_tokens.append(symbol_gen())
+    return new_tokens
