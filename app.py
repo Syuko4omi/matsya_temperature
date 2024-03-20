6675d0f3c42d7e40646f402cb83d40c0fc7f7768
Syuko4omi <riorioblackcat@yahoo.co.jp>
2024-03-20 22:55:19 +0900

    ⚡️ create flask app

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+from matsya_temperature.generate_sentence import generate_sentence
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def matsya_report_temperature():
+    return generate_sentence()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
